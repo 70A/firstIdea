0217105bec54e2614d73104896e2143b3f7f9ba3
70A <1594603348@qq.com>
2018-08-26 17:01:20 +0800

    测试idea上传到git本地仓库

--- src/com/cy/demo/Test.java
+++ src/com/cy/demo/Test.java
@@ -1,5 +1,7 @@
 package com.cy.demo;
 
+import java.sql.*;
+
 /**
  * Created by Tyki bom on 2018/8/26.
  */
@@ -7,5 +9,32 @@ public class Test {
 
     public static void main(String[] args){
         System.out.print("Hello IntelliJ IDEA");
-    }
+        System.out.println(Test.class);
+
+
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/mybatis";
+            Connection conn = DriverManager.getConnection(url,"root","root");
+
+            String sql = "select * from student where spwd = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+         //   ps.setInt(1,1);
+            ps.setString(1,"123");
+            rs = ps.executeQuery();
+            while(rs.next()) {
+                System.out.println(rs.getInt("sid") + rs.getString("sname"));
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        }
+
+
+        }
+
+
 }
