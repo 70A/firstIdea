5109843b61d6018c707c8e79045249f85b3eee80
Ryuuji <1594603348@qq.com>
2018-08-26 16:01:21 +0800

    Initial commit

---
+++ src/com/cy/demo/Test.java
@@ -0,0 +1,11 @@
+package com.cy.demo;
+
+/**
+ * Created by Tyki bom on 2018/8/26.
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.print("Hello IntelliJ IDEA");
+    }
+}
