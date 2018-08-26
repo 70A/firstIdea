f78d02ed02f3856d54c7465aace0b447841d6fff
70A <1594603348@qq.com>
2018-08-27 00:26:50 +0800

    插入排序

---
+++ src/com/cy/demo/InsertSort.java
@@ -0,0 +1,33 @@
+package com.cy.demo;
+
+/**
+ * Created by Tyki bom on 2018/8/27.
+ */
+public class InsertSort {
+
+    public static void main(String[] args){
+        int arr[] = {1, 5, 3, 6, 4, 11, 7, 2, 17};
+        insertSort(arr);
+        System.out.println("选择排序后的数组为：");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void insertSort(int[] arr){
+        if (arr == null || arr.length<2){
+            return;
+        }else {
+            for (int i=1;i<arr.length;i++){
+                for (int j=i-1;j>=0&&arr[j]>arr[j+1];j--){
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int j, int i) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
